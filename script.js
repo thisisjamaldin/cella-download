b322165d29c354c9fccd55957cf301d551a7a782
Jamaldin <47467889+thisisjamaldin@users.noreply.github.com>
2026-03-27 14:32:18 +0600

    Remove web click handling in refresh function
    Remove web click handling from refresh function.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const android = document.getElementById("android")
 const IOS = document.getElementById("IOS")
-const web = document.getElementById("web")
 
 function refresh() {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
@@ -9,8 +8,6 @@ function refresh() {
         android.click()
     }else if(userAgent.includes("iPhone") || userAgent.includes("iPad") || userAgent.includes("iPod")) {
         IOS.click()
-    }else {
-        web.click()
     }
 }
 
